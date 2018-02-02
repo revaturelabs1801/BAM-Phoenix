6fca8a956da835f49646adaa070daf61ea310bbc
Charlie Harris <harrischarliep@gmail.com>
2018-02-02 13:01:22 -0500

    Updated CalendarController for conversion to spring boot: 	-Changed all urls to lowercase 	-Changed @RequestMapping to @GetMapping, @PostMapping, etc. 	-Removed 'produces' property of request mapping annotations 	-All methods now return a ResponseEntity with an appropriate HTTP status code 	-Added null check for get request parameters, methods now send status 400 (bad request) if a required parameter is missing 	-Restructured the changeTopicDate() and updateTopicStatus() methods because they were confusing and hard to read, these methods still behave exactly as they did before 	-Added javadocs to methods that did not already have them, updated javadocs for those that did to include the required parameters

--- BAM/src/main/java/com/revature/bam/controller/CalendarController.java
+++ BAM/src/main/java/com/revature/bam/controller/CalendarController.java
@@ -11,10 +11,12 @@ import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,161 +28,212 @@ import com.revature.bam.service.SubtopicService;
 import com.revature.bam.service.TopicService;
 
 @RestController
-@RequestMapping(value = "Calendar/")
+@RequestMapping(value = "calendar/")
 public class CalendarController {
 
-  private static final String BATCHID = "batchId";
-  private static final String PAGENUMBER = "pageNumber";
-  private static final String PAGESIZE = "pageSize";
+	private static final String BATCHID = "batchId";
+	private static final String PAGENUMBER = "pageNumber";
+	private static final String PAGESIZE = "pageSize";
+	private static final String SUBTOPICID = "subtopicId";
+	private static final String DATE = "date";
+	private static final String STATUS = "status";
 
-  @Autowired
-  SubtopicService subtopicService;
+	@Autowired
+	SubtopicService subtopicService;
 
-  @Autowired
-  TopicService topicService;
+	@Autowired
+	TopicService topicService;
 
-  /**
-   * 
-   * This uses pagination.
-   * Will return a list of subtopics depending on what page and how many
-   * per page of subtopics you want. The page number and size is determined
-   * by the parameters.
-   * 
-   * Depending on how the FullCalendar API is setup to take pages of json
-   * data, this method may need to change.
-   * 
-   * @param request
-   *          - HttpServletRequest
-   * @return
-   *         List<Stubtopic>
-   * 
-   *         Authors: Michael Garza
-   *         Gary LaMountain
-   * 
-   *         note: It will be better to sort by subtopicDate because it will
-   *         load the most
-   *         recent subtopics. However, since the subtopics have the sames
-   *         dates, it's
-   *         causing duplications on the calendar.
-   *         return subtopicService.findByBatchId(batchId, new
-   *         PageRequest(pageNum,pageSiz, Direction.DESC, "subtopicDate"));
-   */
-  @RequestMapping(value = "SubtopicsPagination", method = RequestMethod.GET, produces = "application/json")
-  @ResponseBody
-  public List<Subtopic> getSubtopicsByBatchPagination(HttpServletRequest request) {
-    int batchId = Integer.parseInt(request.getParameter(BATCHID));
-    int pageNum = Integer.parseInt(request.getParameter(PAGENUMBER));
-    int pageSiz = Integer.parseInt(request.getParameter(PAGESIZE));
+	/**
+	 * 
+	 * This uses pagination. Will return a list of subtopics depending on what page
+	 * and how many per page of subtopics you want. The page number and size is
+	 * determined by the parameters.
+	 * 
+	 * Depending on how the FullCalendar API is setup to take pages of json data,
+	 * this method may need to change.
+	 * 
+	 * @param request
+	 *            - Parameters: batchId (int), pageNumber (int), pageSize (int)
+	 * @return List<Stubtopic> , HttpStatus.OK (200) if successful, BAD REQUEST (400)
+	 *         if missing parameters
+	 * 
+	 *         Authors: Michael Garza Gary LaMountain
+	 * 
+	 *         note: It will be better to sort by subtopicDate because it will load
+	 *         the most recent subtopics. However, since the subtopics have the
+	 *         sames dates, it's causing duplications on the calendar. return
+	 *         subtopicService.findByBatchId(batchId, new
+	 *         PageRequest(pageNum,pageSiz, Direction.DESC, "subtopicDate"));
+	 */
+	@GetMapping("subtopicspagination")
+	public ResponseEntity<List<Subtopic>> getSubtopicsByBatchPagination(HttpServletRequest request) {
+		String batchIdParam = request.getParameter(BATCHID);
+		String pageNumParam = request.getParameter(PAGENUMBER);
+		String pageSizeParam = request.getParameter(PAGESIZE);
+		if (batchIdParam == null || pageNumParam == null || pageSizeParam == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 
-    return subtopicService.findByBatchId(batchId, new PageRequest(pageNum, pageSiz));
-  }
+		int batchId = Integer.parseInt(batchIdParam);
+		int pageNum = Integer.parseInt(pageNumParam);
+		int pageSiz = Integer.parseInt(pageSizeParam);
 
-  @RequestMapping(value = "Subtopics", method = RequestMethod.GET, produces = "application/json")
-  @ResponseBody
-  public List<Subtopic> getSubtopicsByBatch(HttpServletRequest request) {
+		return new ResponseEntity<List<Subtopic>>(
+				subtopicService.findByBatchId(batchId, new PageRequest(pageNum, pageSiz)), HttpStatus.OK);
+	}
 
-    // Get the batch id from the request
-    int batchId = Integer.parseInt(request.getParameter(BATCHID));
+	/**
+	 * Gets a list of subtopics for a given batch
+	 * 
+	 * @param request
+	 *            -Parameters: batchId (int)
+	 * @return List of subtopics for the batch with the given id OK (200) if
+	 *         successful, BAD REQUEST (400) if missing parameters
+	 */
+	@GetMapping("subtopics")
+	public ResponseEntity<List<Subtopic>> getSubtopicsByBatch(HttpServletRequest request) {
+		String batchIdParam = request.getParameter(BATCHID);
+		if (batchIdParam == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 
-    // Retrieve and return users in a batch from the database
+		int batchId = Integer.parseInt(batchIdParam);
+		
+		return new ResponseEntity<List<Subtopic>>(subtopicService.getSubtopicByBatchId(batchId), HttpStatus.OK);
+	}
 
-    return subtopicService.getSubtopicByBatchId(batchId);
-  }
+	/**
+	 * Counts the number of Subtopics by matching their ids with the batchId.
+	 * 
+	 * @param request
+	 *            -Parameters: batchId (int)
+	 * @return number(Long) of Subtopics, OK (200) if successful, BAD REQUEST (400)
+	 *         if missing parameters
+	 * @author Michael Garza, Gary LaMountain
+	 */
+	@GetMapping("getnumberofsubtopics")
+	public ResponseEntity<Long> getNumberOfSubTopicsByBatch(HttpServletRequest request) {
+		String batchIdParam = request.getParameter(BATCHID);
+		if (batchIdParam == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 
-  /**
-   * Counts the number of Subtopics by matching their ids with the batchId.
-   * 
-   * @param HttpServletRequest
-   *          object
-   * @return number(Long) of Subtopics
-   * 
-   * @author Michael Garza, Gary LaMountain
-   */
-  @RequestMapping(value = "GetNumberOfSubtopics", method = RequestMethod.GET, produces = "application/json")
-  @ResponseBody
-  public Long getNumberOfSubTopicsByBatch(HttpServletRequest request) {
-    int batchId = Integer.parseInt(request.getParameter(BATCHID));
+		int batchId = Integer.parseInt(batchIdParam);
 
-    return subtopicService.getNumberOfSubtopics(batchId);
-  }
+		return new ResponseEntity<Long>(subtopicService.getNumberOfSubtopics(batchId), HttpStatus.OK);
+	}
 
-  @RequestMapping(value = "Topics", method = RequestMethod.GET, produces = "application/json")
-  @ResponseBody
-  public List<TopicWeek> getTopicsByBatchPag(HttpServletRequest request) {
+	/**
+	 * Gets a list of topics for a given batch
+	 * 
+	 * @param request
+	 *            - Parameters: - batchId (int)
+	 * @return List of topics for the batch with the given id, HttpStatus.OK
+	 *         (200) if successful, BAD REQUEST (400) if missing parameters
+	 */
+	@GetMapping("topics")
+	public ResponseEntity<List<TopicWeek>> getTopicsByBatchPag(HttpServletRequest request) {
+		String batchIdParam = request.getParameter(BATCHID);
+		if (batchIdParam == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 
-    // Get the batch id from the request
-    int batchId = Integer.parseInt(request.getParameter(BATCHID));
+		int batchId = Integer.parseInt(batchIdParam);
 
-    // Retrieve and return users in a batch from the database
-    return topicService.getTopicByBatchId(batchId);
-  }
+		return new ResponseEntity<List<TopicWeek>>(topicService.getTopicByBatchId(batchId), HttpStatus.OK);
+	}
+	
+	
+	/**
+	 * Updates the date for the given subtopic in the given batch
+	 * @param request
+	 * 	Parameters: subtopicId (int), batchId (int), date (long/date)
+	 * @return OK (200) if update occurs, NO CONTENT (204) if requested batch/subtopic does not exist, BAD REQUEST (400) if missing parameters
+	 * @throws ParseException
+	 */
+	@GetMapping("dateupdate")
+	public ResponseEntity<?> changeTopicDate(HttpServletRequest request) throws ParseException {
+		String subtopicIdParam = request.getParameter(SUBTOPICID);
+		String batchIdParam = request.getParameter(BATCHID);
+		String newDateParam = request.getParameter(DATE);
+		if (subtopicIdParam == null || batchIdParam == null || newDateParam == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		int subtopicId = Integer.parseInt(request.getParameter("subtopicId"));
+		int batchId = Integer.parseInt(request.getParameter(BATCHID));
+		long newDate = Long.valueOf(newDateParam);
+		
+		List<Subtopic> topics = subtopicService.getSubtopicByBatchId(batchId);
+		for(Subtopic sub: topics) {
+			if (sub.getSubtopicId() == subtopicId) {
+				sub.setSubtopicDate(new Timestamp(newDate + 46800000));
+				subtopicService.updateSubtopic(sub);
+				return new ResponseEntity<>(HttpStatus.OK);
+			}
+		}
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 
-  @RequestMapping(value = "DateUpdate", method = RequestMethod.GET, produces = "application/json")
-  public void changeTopicDate(HttpServletRequest request) throws ParseException {
-    // Get the batch id from the request
-    int subtopicId = Integer.parseInt(request.getParameter("subtopicId"));
 
-    int batchId = Integer.parseInt(request.getParameter(BATCHID));
-    List<Subtopic> topics = subtopicService.getSubtopicByBatchId(batchId);
-    Subtopic sub;
-    for (int i = 0; i < topics.size(); i++) {
-      if (topics.get(i).getSubtopicId() == subtopicId) {
-        sub = topics.get(i);
-        Long newDate = Long.valueOf(request.getParameter("date")) + 46800000;
-        sub.setSubtopicDate(new Timestamp(newDate));
+	/**
+	 * Updates the status of the given subtopic in the given batch
+	 * @param request
+	 * 	Parameters: subtopicId (int), batchId (int), status (string)
+	 * @return
+	 * @throws ParseException
+	 */
+	@GetMapping("statusupdate")
+	public ResponseEntity<?> updateTopicStatus(HttpServletRequest request) throws ParseException {
+		String subtopicIdParam = request.getParameter(SUBTOPICID);
+		String batchIdParam = request.getParameter(BATCHID);
+		String statusParam = request.getParameter(STATUS);
+		if (subtopicIdParam == null || batchIdParam == null || statusParam == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		int subtopicId = Integer.parseInt(subtopicIdParam);
+		int batchId = Integer.parseInt(batchIdParam);
+		SubtopicStatus status = subtopicService.getStatus(statusParam);
 
-        // Update topic in the database
-        subtopicService.updateSubtopic(sub);
-        break;
-      }
-    }
-  }
+		List<Subtopic> topics = subtopicService.getSubtopicByBatchId(batchId);
+		for (Subtopic sub: topics) {
+			if (sub.getSubtopicId() == subtopicId) {
+				sub.setStatus(status);
+				subtopicService.updateSubtopic(sub);
+				return new ResponseEntity<>(HttpStatus.OK);
+			}
+		}
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 
-  @RequestMapping(value = "StatusUpdate", method = RequestMethod.GET, produces = "application/json")
-  public void updateTopicStatus(HttpServletRequest request) throws ParseException {
-    // Get the batch id from the request
-    int subtopicId = Integer.parseInt(request.getParameter("subtopicId"));
+	@PostMapping("addtopics")
+	public ResponseEntity<?> addTopics(@RequestBody String jsonObject, HttpSession session) {
+		List<TopicName> topicsFromStub = null;
 
-    int batchId = Integer.parseInt(request.getParameter(BATCHID));
-    List<Subtopic> topics = subtopicService.getSubtopicByBatchId(batchId);
-    Subtopic sub;
-    SubtopicStatus status = subtopicService.getStatus(request.getParameter("status"));
-    for (int i = 0; i < topics.size(); i++) {
-      if (topics.get(i).getSubtopicId() == subtopicId) {
-        sub = topics.get(i);
-        sub.setStatus(status);
-        // Update topic in the database
-        subtopicService.updateSubtopic(sub);
-        break;
-      }
-    }
-  }
+		ObjectMapper mapper = new ObjectMapper();
+		try {
 
-  @RequestMapping(value = "AddTopics", method = RequestMethod.POST, produces = "application/json")
-  public void addTopics(@RequestBody String jsonObject, HttpSession session) {
-    List<TopicName> topicsFromStub = null;
-
-    ObjectMapper mapper = new ObjectMapper();
-    try {
-
-      topicsFromStub = mapper.readValue(jsonObject,
-          mapper.getTypeFactory().constructCollectionType(List.class, TopicName.class));
-    } catch (IOException e) {
-      LogManager.getRootLogger().error(e);
-    }
-    List<TopicName> allTopicsInBAM = topicService.getTopics();
-    for (int i = 0; i < topicsFromStub.size(); i++) {
-      boolean found = false;
-      for (int j = 0; j < allTopicsInBAM.size(); j++) {
-        if (topicsFromStub.get(i).getName().equals(allTopicsInBAM.get(j).getName())) {
-          found = true;
-          break;
-        }
-      }
-      if (!found) {
-        topicService.addOrUpdateTopicName(topicsFromStub.get(i));
-      }
-    }
-  }
+			topicsFromStub = mapper.readValue(jsonObject,
+					mapper.getTypeFactory().constructCollectionType(List.class, TopicName.class));
+		} catch (IOException e) {
+			LogManager.getRootLogger().error(e);
+		}
+		List<TopicName> allTopicsInBAM = topicService.getTopics();
+		for (int i = 0; i < topicsFromStub.size(); i++) {
+			boolean found = false;
+			for (int j = 0; j < allTopicsInBAM.size(); j++) {
+				if (topicsFromStub.get(i).getName().equals(allTopicsInBAM.get(j).getName())) {
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				topicService.addOrUpdateTopicName(topicsFromStub.get(i));
+			}
+		}
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 }
