70ac828130cb60e702ef0dbd1f8c56fdfd4e9270
jgmacho <jgmacho48@gmail.com>
2018-02-06 10:45:51 -0500

    @pathvariable for usercontroller/topiccontroller/subtopiccontroller

--- BAM/src/main/java/com/revature/bam/controller/SubTopicController.java
+++ BAM/src/main/java/com/revature/bam/controller/SubTopicController.java
@@ -1,15 +1,13 @@
 package com.revature.bam.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.revature.bam.bean.SubtopicName;
 import com.revature.bam.bean.SubtopicType;
 import com.revature.bam.bean.TopicName;
@@ -25,7 +23,6 @@ public class SubTopicController {
 
 	@Autowired
 	SubtopicService subTopicService;
-
 /**
  * @author Cristian Hermida / Batch 1712-dec10-java-steve
  * 
@@ -35,12 +32,11 @@ public class SubTopicController {
  * 			- status of 201 CREATED if a Subtopic is created or updated.
  * 			- status of 204 NO_CONTENT is a Subtopic is not created.
  */
-
-	@PostMapping("add")
-	public ResponseEntity<?> addSubTopicName(HttpServletRequest request) {
-		SubtopicType type = subTopicService.getSubtopicType(Integer.parseInt(request.getParameter("typeId")));
-		TopicName topic = topicService.getTopicName(Integer.parseInt(request.getParameter("topicId")));
-		SubtopicName subtopic = new SubtopicName(request.getParameter("subtopicName"), topic, type);
+	@PostMapping("add/{typeId}/{topicId}/{subtopicName}")
+	public ResponseEntity<?> addSubTopicName(@PathVariable int typeId, @PathVariable int topicId, @PathVariable String subtopicName) {
+		SubtopicType type = subTopicService.getSubtopicType(typeId);
+		TopicName topic = topicService.getTopicName(topicId);
+		SubtopicName subtopic = new SubtopicName(subtopicName, topic, type);
 		SubtopicName topicUpdate = subTopicService.addOrUpdateSubtopicName(subtopic);
 		if (topicUpdate != null) {
 			return new ResponseEntity<>(HttpStatus.CREATED);
@@ -49,5 +45,4 @@ public class SubTopicController {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
 	}
-
 }
