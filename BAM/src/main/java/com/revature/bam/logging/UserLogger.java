d04298a8f700d62c0a37162e726b059042644ebf
Allan <allanjpoindexter@outlook.com>
2018-02-06 22:44:26 -0500

    Re-adds deleted Logger files

---
+++ BAM/src/main/java/com/revature/bam/logging/UserLogger.java
@@ -0,0 +1,41 @@
+package com.revature.bam.logging;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Before;
+
+public class UserLogger {
+//	/* TODO: Remove Sysouts */
+//	
+//	/**
+//	 * Returns the affected class's name and the date and time the method was invoked. 
+//	 * @throws Throwable 
+//	 */
+//	@Around("execution (* com.revature.bam.controller.UserController.*(..))")
+//	public void classNameAndTime(ProceedingJoinPoint pjp) throws Throwable {
+//		String sdf = new SimpleDateFormat("MM-dd-yyyy, hh:mm:ss.SSS a ").format(new Date());
+//		System.out.print(sdf);
+//		System.out.print(" -- UserController -- ");
+//		
+//		long start = System.currentTimeMillis();
+//		pjp.proceed();
+//		long end = System.currentTimeMillis();
+//		System.out.println("Total time: " + (end-start) + " milliseconds.");
+//		
+//	}
+//	
+//	@Before("execution (* com.revature.bam.controller.UserController.*(..))")
+//	public void beforeMethod(JoinPoint jp) {
+//		System.out.println("Before " + jp.getSignature().getName() + " method.");
+//	}
+//	
+//	@After("execution (* com.revature.bam.controller.UserController.*(..))")
+//	public void afterMethod(JoinPoint jp) {
+//		System.out.println("After " + jp.getSignature().getName() + " method.");
+//	}
+}
