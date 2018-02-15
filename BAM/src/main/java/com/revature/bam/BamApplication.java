199e9e51fb11bc49c4c00d322475309466c2604e
Mohamed <mhswelam@outlook.com>
2018-02-15 16:30:04 -0500

    clear not needed space

--- BAM/src/main/java/com/revature/bam/BamApplication.java
+++ BAM/src/main/java/com/revature/bam/BamApplication.java
@@ -9,8 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BamApplication.class, args);
-		
-
+		SpringApplication.run(BamApplication.class, args);	
 	}
 }
